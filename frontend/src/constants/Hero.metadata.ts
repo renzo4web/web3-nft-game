dc54bdf910dccff66236a12147d8b5201a6156c9
Renzo Barrios <renzo.barrios@blackbox-vision.tech>
2022-06-10 20:07:10 -0300

    feat: added frontend in vite

---
+++ frontend/src/constants/Hero.metadata.ts
@@ -0,0 +1,23 @@
+export const HEROES_METADATA = [
+  {
+    classHero: "Barbarian",
+    imageURI:
+      "https://cdn2.iconfinder.com/data/icons/greek-gods-and-goddesses/307/greek-god-mythology-018-512.png",
+    bgColor: "tomato",
+    colorBadge: "orange",
+  },
+  {
+    classHero: "Mage",
+    imageURI:
+      "https://cdn0.iconfinder.com/data/icons/game-elements/64/mage-avatar-mystery-user-magician-512.png",
+    bgColor: "cyan.400",
+    colorBadge: "linkedin",
+  },
+
+  {
+    classHero: "Healer",
+    imageURI: "https://static.thenounproject.com/png/1357494-200.png",
+    bgColor: "green.400",
+    colorBadge: "whatsapp",
+  },
+];
